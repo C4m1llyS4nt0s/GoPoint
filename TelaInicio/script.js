b6a90ee3d918688f850e57acd7e86460cd5d302b
camis <145611862+C4m1llyS4nt0s@users.noreply.github.com>
2025-02-12 09:40:00 -0300

    Aplicação da api

--- TelaInicio/script.js
+++ TelaInicio/script.js
@@ -1,56 +1,73 @@
 const users = [
-  { name: 'Usuário1', module: 'Módulo X' },
-  { name: 'Usuário2', module: 'Módulo Y' },
-  { name: 'Usuário3', module: 'Módulo Z' },
-];
+    { name: 'Usuário1', module: 'Módulo X' },
+    { name: 'Usuário2', module: 'Módulo Y' },
+    { name: 'Usuário3', module: 'Módulo Z' },
+  ];
+  
+  const searchInput = document.getElementById('searchInput');
+  
+  function filterUsers() {
+      const searchValue = searchInput.value.toLowerCase();
+      const userList = document.getElementById('userList');
+      const body = document.body;
+  
+      // Controlar estados
+      if(searchValue.length > 0) {
+          body.classList.add('search-active');
+      } else {
+          body.classList.remove('search-active'); 
+      }
+  
+      // Filtragem
+      userList.innerHTML = '';
+      const filteredUsers = users.filter(user => 
+          user.name.toLowerCase().includes(searchValue)
+      );
+  
+      filteredUsers.forEach(user => {
+          const userCard = document.createElement('div');
+          userCard.className = 'user-card';
+          userCard.innerHTML = `
+              <div class="info">
+                  <span class="name">${user.name}</span>
+                  <span class="module">${user.module}</span>
+              </div>
+              <button onclick="goToNewScreen(${users.indexOf(user)})">Acessar</button>
+          `;
+          userList.appendChild(userCard);
+      });
+  }
+  
+  function goToNewScreen(userIndex) {
+    const user = users[userIndex];
+    localStorage.setItem('selectedUser', JSON.stringify(user));
+    window.location.href = '../TelaPonto/TelaPonto.html';
+  }
+  
+  // Eventos de focus/blur
+  searchInput.addEventListener('focus', () => {
+      document.body.classList.add('search-active');
+  });
+  
+  searchInput.addEventListener('blur', () => {
+      if(!searchInput.value) {
+          document.body.classList.remove('search-active');
+      }
+  });
+  const URL = "http://localhost:3000/api/users";
 
-const searchInput = document.getElementById('searchInput');
+  async function chamarApi() {
+    try {
+      const resp = await fetch(URL);
+      if (!resp.ok) {
+        throw new Error(`Erro HTTP! Status: ${resp.status}`);
+      }
+      const obj = await resp.json();
+      console.log(obj);  
+    } catch (error) {
 
-function filterUsers() {
-    const searchValue = searchInput.value.toLowerCase();
-    const userList = document.getElementById('userList');
-    const body = document.body;
-
-    // Controlar estados
-    if(searchValue.length > 0) {
-        body.classList.add('search-active');
-    } else {
-        body.classList.remove('search-active'); 
+      console.error("Falha na requisição:", error);
     }
-
-    // Filtragem
-    userList.innerHTML = '';
-    const filteredUsers = users.filter(user => 
-        user.name.toLowerCase().includes(searchValue)
-    );
-
-    filteredUsers.forEach(user => {
-        const userCard = document.createElement('div');
-        userCard.className = 'user-card';
-        userCard.innerHTML = `
-            <div class="info">
-                <span class="name">${user.name}</span>
-                <span class="module">${user.module}</span>
-            </div>
-            <button onclick="goToNewScreen(${users.indexOf(user)})">Acessar</button>
-        `;
-        userList.appendChild(userCard);
-    });
-}
-
-function goToNewScreen(userIndex) {
-  const user = users[userIndex];
-  localStorage.setItem('selectedUser', JSON.stringify(user));
-  window.location.href = '../TelaPonto/TelaPonto.html';
-}
-
-// Eventos de focus/blur
-searchInput.addEventListener('focus', () => {
-    document.body.classList.add('search-active');
-});
-
-searchInput.addEventListener('blur', () => {
-    if(!searchInput.value) {
-        document.body.classList.remove('search-active');
-    }
-});
+  }
+  
+  chamarApi();
